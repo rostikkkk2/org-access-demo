28b9aaff817ab4a78892de58b686b489b3f2a7d1
Rostyslav Yuriev <rostyslav.y@allerin.nl>
2025-07-10 22:22:20 +0300

    Initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+import "@hotwired/turbo-rails"
+
+import "trix"
+import "@rails/actiontext"
+
+import "./controllers"
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './app/views/**/*.haml',
+    './app/views/**/*.html.erb',
+    './app/views/**/*.html.haml',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ]
+}
